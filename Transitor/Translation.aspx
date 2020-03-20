613459b87fc78729a2a2f06f0be49e94d25b756f
Fulop Hunor <fulophunor12@gmail.com>
2020-03-20 13:08:40 +0200

    Started working on translation page

--- Transitor/Translation.aspx
+++ Transitor/Translation.aspx
@@ -3,13 +3,75 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/> 
+<style>
+    body {
+      font-family: Arial;
+      color: white;
+    }
+
+    .split {
+      height: 100%;
+      width: 50%;
+      position: fixed;
+      z-index: 1;
+      top: 0;
+      overflow-x: hidden;
+      padding-top: 20px;
+    }
+
+    .left {
+      left: 0;
+      background-color: #111;
+    }
+
+    .right {
+      right: 0;
+      background-color: red;
+    }
+
+    .centered {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+    }
+
+    .centered img {
+      width: 150px;
+      border-radius: 50%;
+    }
+    </style>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+
+    <div class="split left">
+      <div class="centered">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" OnRowCommand="GridView1_RowCommand">
+        <Columns>
+            <asp:BoundField DataField="Phrase" HeaderText="Phrase" ItemStyle-Width="150px" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button Text="Select" runat="server" CommandName="Select" CommandArgument="<%# Container.DataItemIndex %>" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        </asp:GridView>
+      </div>
+    </div>
+
+    <div class="split right">
+      <div class="centered">
+        <asp:TextBox ID="TextBoxPhrase" runat="server"></asp:TextBox>
+        <asp:Label ID="LabelTranslatedPhrase" runat="server" Text="Translate the pharese here"></asp:Label>
+        <asp:TextBox ID="TextBoxTranslatedPhrase" runat="server"></asp:TextBox>
+        <p>
+          <asp:Button ID="btnTranslate" runat="server" Text="Translate" />
+        </p>
+      </div>
+    </div>
+     
 </body>
-</html>
+</html> 
--- Transitor/Translation.aspx.cs
+++ Transitor/Translation.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +12,70 @@ namespace Transitor
 {
     public partial class Translation : System.Web.UI.Page
     {
+        string projectID;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                projectID = getId();
+                getData(projectID);
+            }
+        }
 
+        string getId()
+        {
+            string temp = "";
+            string connectionString = WebConfigurationManager.ConnectionStrings["MyDbConn"].ConnectionString;
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            string query = "SELECT ProjectID FROM tblProjects WHERE ProjectName = @ProjectName";
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCmd.Parameters.AddWithValue("@ProjectName", Request.QueryString["test"]);
+            SqlDataReader nwReader = sqlCmd.ExecuteReader();
+            while (nwReader.Read())
+            {
+                temp = nwReader["ProjectID"].ToString();
+            }
+            nwReader.Close();
+            sqlCon.Close();
+            return temp;
+        }
+
+        void getData(string projectID)
+        {
+            string connectionString1 = WebConfigurationManager.ConnectionStrings["MyDbConn"].ConnectionString;
+            SqlConnection sqlCon1 = new SqlConnection(connectionString1);
+            string query1 = "SELECT Phrase FROM tblPhrase WHERE ProjectID = @ProjectID";
+            sqlCon1.Open();
+            SqlCommand sqlCmd1 = new SqlCommand(query1, sqlCon1);
+            sqlCmd1.Parameters.AddWithValue("@ProjectID", projectID);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCmd1);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+            sqlCon1.Close();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+                //Determine the RowIndex of the Row whose Button was clicked.
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+                //Reference the GridView Row.
+                GridViewRow row = GridView1.Rows[rowIndex];
+
+                //Fetch value of Name.
+                TextBoxPhrase.Text = row.Cells[0].Text;
+            }
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Confirms that an HtmlForm control is rendered for the specified ASP.NET
+               server control at run time. */
         }
     }
 }
